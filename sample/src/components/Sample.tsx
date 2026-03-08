58125778d9708d16b8c7edd28bd0e528e0f31fc5
Akash <aakash.velu.003@gmail.com>
2026-03-08 14:31:16 +0530

    REFINE

--- sample/src/components/Sample.tsx
+++ sample/src/components/Sample.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { use, useState } from "react";
 
 interface sampleProps {
   name: string;
@@ -9,13 +10,29 @@ interface sampleProps {
 }
 
 const Sample = ({ name, id, price, show, msg }: sampleProps) => {
+  const [products, setProducts] = useState(false);
+
+  const handleClick = (name: string, price: number) => {
+    setProducts(true);
+    console.log(`${name} purchased with ${price}`);
+  };
+
   return (
     <div>
       <div className="border border-gray-300 rounded-lg p-4 m-2 w-48 shadow-md bg-white">
         <h1 className="text-blue-500 text-xl font-bold mb-2"> {name}</h1>
         <p className="text-green-500 text-base mb-1"> ID:{id}</p>
-        <p className="ext-red-500 text-base"> Price:{price}</p>
-        <p className="text-gray-500 text-base"> Message: {msg}</p>
+        <p className="text-red-500 text-base"> Price:{price}</p>
+        <p className="text-gray-500 text-base">
+          {" "}
+          Message: {products ? "Purchased" : ""}
+        </p>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => handleClick(name, price)}
+        >
+          Buy Now
+        </button>
       </div>
     </div>
   );
